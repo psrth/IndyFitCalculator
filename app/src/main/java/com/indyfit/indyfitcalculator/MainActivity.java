e89b6f182879c9db31ab27059b41bdeabcd8760d
Parth Sharma <parthsharma151@gmail.com>
2020-08-23 16:59:12 +0530

    recycler view and relevant adapter

--- app/src/main/java/com/indyfit/indyfitcalculator/MainActivity.java
+++ app/src/main/java/com/indyfit/indyfitcalculator/MainActivity.java
@@ -1,14 +1,33 @@
 package com.indyfit.indyfitcalculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerView calculatorView;
+
+    String s1[], s2[];
+    int images[] = {R.drawable.img1, R.drawable.img2, R.drawable.img3, R.drawable.img4,
+            R.drawable.img5, R.drawable.img6, R.drawable.img7, R.drawable.img8, R.drawable.img9,
+            R.drawable.img10, R.drawable.img11, R.drawable.img12, R.drawable.img13, R.drawable.img14
+            , R.drawable.img15, R.drawable.img16};
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calculatorView = findViewById(R.id.calculatorView);
+
+        s1 = getResources().getStringArray(R.array.calculator_names);
+        s2 = getResources().getStringArray(R.array.calculator_desc);
+
+        MyAdapter myAdapter = new MyAdapter(this, s1, s2, images);
+        calculatorView.setAdapter(myAdapter);
+        calculatorView.setLayoutManager(new LinearLayoutManager(this));
     }
 }
---
+++ app/src/main/java/com/indyfit/indyfitcalculator/MyAdapter.java
@@ -0,0 +1,64 @@
+package com.indyfit.indyfitcalculator;
+
+import android.content.Context;
+import android.media.Image;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.zip.Inflater;
+
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.MyViewHolder> {
+
+    String data1[], data2[];
+    int images[];
+    Context context;
+
+    public MyAdapter(Context ct, String s1[], String s2[], int imgs[]){
+        context = ct;
+        data1 = s1;
+        data2 = s2;
+        images = imgs;
+
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View view = inflater.inflate(R.layout.my_row, parent, false);
+
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        holder.myText1.setText(data1[position]);
+        holder.myText2.setText(data2[position]);
+        holder.imageView.setImageResource(images[position]);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return images.length;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder{
+
+        TextView myText1, myText2;
+        ImageView imageView;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            myText1 = itemView.findViewById(R.id.exercise_text);
+            myText2 = itemView.findViewById(R.id.desc_text);
+            imageView = itemView.findViewById(R.id.myImageView);
+        }
+    }
+}
